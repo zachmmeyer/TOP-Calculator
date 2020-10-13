7820e0dc0bda690d01e18c48e7027e11d4916b6b
Zach Meyer <zachary.michael.meyer@gmail.com>
2020-10-13 17:30:49 -0400

    Multiple functions for calculator via keyboard

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const displayedNumber = document.querySelector(".calcDisplay");
-
 function add(a, b) {
   return a + b;
 }
@@ -16,6 +14,11 @@ function divide(a, b) {
   return a / b;
 }
 
+function buttonKey(key) {
+  let keyPress = new KeyboardEvent("keydown", { key: key });
+  document.dispatchEvent(keyPress);
+}
+
 function combineValue(currentNumber, inputNumber) {
   if (currentNumber === 0) {
     return inputNumber;
@@ -25,21 +28,64 @@ function combineValue(currentNumber, inputNumber) {
 }
 
 function calculator() {
-  displayedNumber.textContent = "Hello";
+  const currentNumberDisplay = document.querySelector(".calcDisplay");
+  const queuedNumberDisplay = document.querySelector(".calcQueue");
 
   let currentNumber = 0;
-  displayedNumber.textContent = currentNumber;
+  let queuedNumber = 0;
+  let operatorValue = "";
+  let operatorBool = false;
+  let equationDone = false;
+  currentNumberDisplay.textContent = currentNumber;
 
   window.addEventListener("keydown", function (e) {
-    if (Number(e.key) >= 0 || (Number(e.key) <= 9 && e.key != " ")) {
-      currentNumber = combineValue(currentNumber, e.key);
-      displayedNumber.textContent = currentNumber;
-      console.log(currentNumber);
-      console.log(displayedNumber);
+    console.log(e);
+    if (equationDone === false) {
+      if (Number(e.key) >= 0 || (Number(e.key) <= 9 && e.key != " ")) {
+        currentNumber = combineValue(currentNumber, e.key);
+        currentNumberDisplay.textContent = currentNumber;
+      } else if (operatorBool === false) {
+        if (e.key === "*" || e.key === "/" || e.key === "+" || e.key === "-") {
+          operatorBool = true;
+          operatorValue = e.key;
+          queuedNumber = currentNumber;
+          queuedNumberDisplay.textContent =
+            currentNumberDisplay.textContent + " " + operatorValue;
+          currentNumber = 0;
+        }
+      }
+      if (e.key === "Enter" && operatorBool === true) {
+        //queuedNumberDisplay.textContent += " " + currentNumberDisplay.textContent;
+        queuedNumberDisplay.textContent +=
+          " " + currentNumberDisplay.textContent;
+        switch (operatorValue) {
+          case "*":
+            currentNumber = multiply(currentNumber, queuedNumber);
+            break;
+          case "/":
+            currentNumber = divide(currentNumber, queuedNumber);
+            break;
+          case "+":
+            currentNumber = add(currentNumber, queuedNumber);
+            break;
+          case "-":
+            currentNumber = subtract(currentNumber, queuedNumber);
+            break;
+        }
+        currentNumberDisplay.textContent = currentNumber;
+        equationDone = true;
+      }
+    }
+    if (e.key === "Escape") {
+      currentNumber = 0;
+      queuedNumber = 0;
+      operatorValue = "";
+      currentNumberDisplay.textContent = 0;
+      queuedNumberDisplay.textContent = "";
+      operatorBool = false;
+      equationDone = false;
     }
   });
 }
 
 calculator();
-
-//TODO Make function to add number to string or array
