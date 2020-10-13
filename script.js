ac7171eba0ae5caf61245ad7686aa937a08b442a
Zach Meyer <zachary.michael.meyer@gmail.com>
2020-10-13 14:55:35 -0400

    Ability to add numbers to calc display

--- script.js
+++ script.js
@@ -16,12 +16,30 @@ function divide(a, b) {
   return a / b;
 }
 
-displayedNumber.textContent = "Hello";
-
-window.addEventListener("keydown", function (e) {
-  if (Number(e.key) >= 0 || Number(e.key) <= 9) {
-    displayedNumber.textContent = e.key;
+function combineValue(currentNumber, inputNumber) {
+  if (currentNumber === 0) {
+    return inputNumber;
   }
-});
+  combinedValue = Number(currentNumber.toString() + inputNumber);
+  return Number.isSafeInteger(combinedValue) ? combinedValue : currentNumber;
+}
+
+function calculator() {
+  displayedNumber.textContent = "Hello";
+
+  let currentNumber = 0;
+  displayedNumber.textContent = currentNumber;
+
+  window.addEventListener("keydown", function (e) {
+    if (Number(e.key) >= 0 || (Number(e.key) <= 9 && e.key != " ")) {
+      currentNumber = combineValue(currentNumber, e.key);
+      displayedNumber.textContent = currentNumber;
+      console.log(currentNumber);
+      console.log(displayedNumber);
+    }
+  });
+}
+
+calculator();
 
 //TODO Make function to add number to string or array
